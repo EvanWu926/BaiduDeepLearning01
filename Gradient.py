913149a11a01de08405812469b213ef81eee508d
evan.wu <252392785@qq.com>
2022-11-25 11:00:43 +0800

    Gradient

---
+++ Gradient.py
@@ -0,0 +1,110 @@
+import numpy as np
+
+def load_data():
+    # 从文件导入数据
+    datafile = 'D:\BaiduSyncdisk\Evan\Evan\Evan\Work\BaiduDeepLearning01\data\p1\housing.data'
+    data = np.fromfile(datafile, sep=' ')
+
+    # 每条数据包括14项，其中前面13项是影响因素，第14项是相应的房屋价格中位数
+    feature_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', \
+                     'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+    feature_num = len(feature_names)
+
+    # 将原始数据进行Reshape，变成[N, 14]这样的形状
+    data = data.reshape([data.shape[0] // feature_num, feature_num])
+
+    # 将原数据集拆分成训练集和测试集
+    # 这里使用80%的数据做训练，20%的数据做测试
+    # 测试集和训练集必须是没有交集的
+    ratio = 0.8
+    offset = int(data.shape[0] * ratio)
+
+    # 计算训练集的最大值，最小值，平均值
+    maximums, minimums, avgs = data.max(axis=0), data.min(axis=0), \
+                               data.sum(axis=0) / data.shape[0]
+
+    # 对数据进行归一化处理
+    for i in range(feature_num):
+        # print(maximums[i], minimums[i], avgs[i])
+        data[:, i] = (data[:, i] - minimums[i]) / (maximums[i] - minimums[i])
+        # print(data)
+
+    # 训练集和测试集的划分比例
+    training_data = data[:offset]
+    testing_data = data[offset:]
+    return training_data, testing_data
+
+
+
+
+
+
+class Network(object):
+    def __init__(self, num_of_weights):
+        # 随机产生w的初始值
+        # 为了保持程序每次运行结果的一致性，
+        # 此处设置固定的随机数种子
+        np.random.seed(0)
+        self.w = np.random.randn(num_of_weights, 1)
+        self.b = 0.0
+
+    # 前向计算，公式XA+B
+    def forward(self, x):
+        z = np.dot(x, self.w) + self.b
+        return z
+
+    # 误差函数(Z-Y)^2
+    def loss(self, y, z):
+        error = z - y
+        cost = error ** 2
+
+        return np.mean(cost)
+
+    def gradient(self, x, y):
+        z = self.forward(x)
+        gradient_w = (z - y) * x
+        gradient_w = np.mean(gradient_w, axis=0)
+        gradient_w = gradient_w[:, np.newaxis]
+        gradient_b = (z - y)
+        gradient_b = np.mean(gradient_b)
+
+        return gradient_w, gradient_b
+
+
+
+# net = Network(13)
+#
+# # 数据获取
+# train_data, test_data = load_data()
+# ix = train_data[:, :-1]
+# iy = train_data[:, -1:]
+# iz = net.forward(ix)
+#
+# # 取第一个数据作为样本
+# x1 = ix[0]
+# y1 = ix[0]
+# z1 = net.forward(x1)
+#
+# # for x_i in x1:
+# #     gradient_w = (z1 - y1) * x_i
+# #     print('gradient_w {}'.format(gradient_w))
+#
+# # gradient_w = (z1 - y1) * x1
+# # print('gradient_w_by_sample1 {}, gradient.shape {}'.format(gradient_w, gradient_w.shape))
+#
+# # x3sample = x[0:3]
+# # y3sample = y[0:3]
+# # z3sample = net.forward(x3sample)
+# #
+# # gradient_w = (z3sample - y3sample) * x3sample
+# # print('gradient_w {}, gradient_shape {}'.format(gradient_w, gradient_w.shape))
+#
+# gradient_w = (iz - iy) * ix
+# # print(gradient_w)
+# # axis = 0 表示把每一行做相加然后再除以总的行数
+# gradient_w = np.mean(gradient_w, axis=0)
+# gradient_w = gradient_w[:, np.newaxis]
+#
+# gradient_b  = (iz - iy)
+# gradient_b = np.mean(gradient_b)
+# # print(gradient_b)
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import test123
+# import Gradient
 
-d2o = test123.Data2One()
 
 # 获取数据
-training_data, test_data = d2o.load_data()
-x = training_data[:, :-1]
-y = training_data[:, -1:]
+# training_data, test_data = Gradient.load_data()
+# x = training_data[:, :-1]
+# y = training_data[:, -1:]
 # print(x[0])
 # print(y[0])
 
@@ -19,29 +18,61 @@ y = training_data[:, -1:]
 # print(loss)
 
 
-net = test123.Network(13)
+# net = Gradient.Network(13)
+#
+# # 只画出参数w5和w9在区间[-160, 160]的曲线部分，以及包含损失函数的极值
+# w5 = Network.np.arange(-160.0, 160.0, 1.0)
+# w9 = test123.np.arange(-160.0, 160.0, 1.0)
+# losses = test123.np.zeros([len(w5), len(w9)])
+#
+# # 计算设定区域内每个参数取值所对应的Loss
+# for i in range(len(w5)):
+#     for j in range(len(w9)):
+#         net.w[5] = w5[i]
+#         net.w[9] = w9[j]
+#         z = net.forward(x)
+#         loss = Gradient.loss(z, y)
+#         losses[i, j] = loss
+#
+# # 使用matplotlib将两个变量和对应的Loss作3D图
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+#
+# ax = plt.figure().add_subplot(projection='3d')
+#
+# w5, w9 = Gradient.np.meshgrid(w5, w9)
+#
+# ax.plot_surface(w5, w9, losses, rstride=1, cstride=1, cmap = 'rainbow')
+# plt.show()
 
-# 只画出参数w5和w9在区间[-160, 160]的曲线部分，以及包含损失函数的极值
-w5 = test123.np.arange(-160.0, 160.0, 1.0)
-w9 = test123.np.arange(-160.0, 160.0, 1.0)
-losses = test123.np.zeros([len(w5), len(w9)])
 
-# 计算设定区域内每个参数取值所对应的Loss
-for i in range(len(w5)):
-    for j in range(len(w9)):
-        net.w[5] = w5[i]
-        net.w[9] = w9[j]
-        z = net.forword(x)
-        loss = test123.loss(z, y)
-        losses[i, j] = loss
 
-# 使用matplotlib将两个变量和对应的Loss作3D图
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+"""
+梯度计算
+"""
+import Gradient
 
-ax = plt.figure().add_subplot(projection='3d')
 
-w5, w9 = test123.np.meshgrid(w5, w9)
+# 获取数据
+training_data, test_data = Gradient.load_data()
+x = training_data[:, :-1]
+y = training_data[:, -1:]
 
-ax.plot_surface(w5, w9, losses, rstride=1, cstride=1, cmap = 'rainbow')
-plt.show()
+
+net = Gradient.Network(13)
+# 设置[w5, w9] = [-100., -100.]
+net.w[5] = -100.0
+net.w[9] = -100.0
+
+z = net.forward(x)
+losses = net.loss(y, z)
+gradient_w, gradient_b = net.gradient(x, y)
+
+gradient_w5 = gradient_w[5][0]
+gradient_w9 = gradient_w[9][0]
+print('point {}, losses {}'.format([net.w[5][0], net.w[9][0]], losses))
+print('gradient {}'.format([gradient_w5, gradient_w9]))
+
+
+
+
